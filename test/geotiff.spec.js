b43a98da3c668e8ee23fa6c32eda2c9616f55467
Rdataflow <waldgis@bafu.admin.ch>
2025-01-08 21:33:16 +0100

    reenable only new test 2

--- test/geotiff.spec.js
+++ test/geotiff.spec.js
@@ -247,10 +247,12 @@ describe('GeoTIFF', () => {
     await performTiffTests(tiff, 539, 448, 15, Uint16Array);
   });
 
+  /*
   it('should work on Zstandard compressed tiffs', async () => {
     const tiff = await GeoTIFF.fromSource(createSource('zstd.tiff'));
     await performTiffTests(tiff, 539, 448, 15, Uint16Array);
   });
+  */
 
   it('should work on Float32 and LERC compressed tiffs', async () => {
     const tiff = await GeoTIFF.fromSource(createSource('float32lerc.tiff'));
@@ -270,12 +272,11 @@ describe('GeoTIFF', () => {
     const tiff = await GeoTIFF.fromSource(createSource('float32lerc_zstd.tiff'));
     await performTiffTests(tiff, 539, 448, 15, Float32Array);
   });
-  /*
+
   it('should work on Float32 and Zstandard compressed tiffs', async () => {
     const tiff = await GeoTIFF.fromSource(createSource('float32zstd.tiff'));
     await performTiffTests(tiff, 539, 448, 15, Float32Array);
   });
-  */
 
   it('should work with worker pool', async () => {
     const testPool = new Pool();
