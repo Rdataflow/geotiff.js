9fb1c7db03c132d500a467ac4dc67e966d38bff5
Rdataflow <waldgis@bafu.admin.ch>
2025-01-08 21:12:12 +0100

    reenable new test 1

--- test/geotiff.spec.js
+++ test/geotiff.spec.js
@@ -246,12 +246,11 @@ describe('GeoTIFF', () => {
     const tiff = await GeoTIFF.fromSource(createSource('lerc_zstd.tiff'));
     await performTiffTests(tiff, 539, 448, 15, Uint16Array);
   });
-  /*
+
   it('should work on Zstandard compressed tiffs', async () => {
     const tiff = await GeoTIFF.fromSource(createSource('zstd.tiff'));
     await performTiffTests(tiff, 539, 448, 15, Uint16Array);
   });
-  */
 
   it('should work on Float32 and LERC compressed tiffs', async () => {
     const tiff = await GeoTIFF.fromSource(createSource('float32lerc.tiff'));
