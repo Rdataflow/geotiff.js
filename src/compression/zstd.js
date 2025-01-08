6aafba2bcdfc1e2d5a33ff3c93e65fb1d242305f
Rdataflow <waldgis@bafu.admin.ch>
2025-01-08 21:41:16 +0100

    length 0

--- src/compression/zstd.js
+++ src/compression/zstd.js
@@ -5,6 +5,6 @@ export const zstd = new ZSTDDecoder();
 
 export default class ZstdDecoder extends BaseDecoder {
   decodeBlock(buffer) {
-    return zstd.decode(new Uint8Array(buffer)).buffer;
+    return zstd.decode(new Uint8Array(buffer), 0).buffer;
   }
 }
